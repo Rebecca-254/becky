0ceea2a02640c06eef9b06f1be84c3ccb57e25c2
Rebecca-254 <gitaurebecca407@gmail.com>
2025-06-16 13:41:27 +0300

    Add Render domain to ALLOWED_HOSTS

--- becky_site/settings.py
+++ becky_site/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-key-for-dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://beckiedavis.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['https://beckiedavis.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
