102cb7dba71a9c4d8e7f19b3a44283358d44b49b
Rebecca-254 <gitaurebecca407@gmail.com>
2025-06-16 13:23:12 +0300

    Added Render ALLOWED_HOSTS

--- becky_site/settings.py
+++ becky_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iha!roe#n&@%xt$q*z7v*w8k^chyrklpus59(tg(c^4)5jhbx='
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-key-for-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://beckiedavis.onrender.com', '127.0.0.1']
 
 
 # Application definition
