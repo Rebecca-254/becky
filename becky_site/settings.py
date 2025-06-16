900b0b65ecb595621e37f0a333f54c1833712c03
Rebecca-254 <gitaurebecca407@gmail.com>
2025-06-16 16:14:35 +0300

    updated settings.py

--- becky_site/settings.py
+++ becky_site/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-key-for-dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
- ALLOWED_HOSTS = ['https://beckiedavis.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['https://beckiedavis.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
