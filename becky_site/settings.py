4e77e80c6476ac68983f385b921b44f4dd20166c
Rebecca-254 <gitaurebecca407@gmail.com>
2025-06-16 15:30:41 +0300

    updated styles and collect static files

--- becky_site/settings.py
+++ becky_site/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'polls' / 'static',
+]
